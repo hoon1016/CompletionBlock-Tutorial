87652bf8982f1c3ddf62f28d851cc4e6aedf9eaf
hoon1016 <wjsrms76@gmail.com>
2021-08-04 17:17:38 +0900

    swift study

--- CompletionBlock-Tutorial/ViewController.swift
+++ CompletionBlock-Tutorial/ViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
+import KRProgressHUD
+
 class ViewController: UIViewController {
 
+    @IBOutlet var mainTitle: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("ViewController - viewDidLoad() 호출됨")
+        
+        KRProgressHUD.show(withMessage: "Loding")
+        sayHi(completion: { result in
+            print("컴플레션 블락으로 넘겨받았음:\(result)")
+            
+            KRProgressHUD.showSuccess(withMessage: "Success")
+            
+            self.mainTitle.text = result
+        })
+        
     }
 
 
+    fileprivate func sayHi(completion: @escaping(String) -> ()){
+        print("ViewController - sayHiDidLoad() 호출됨")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+               //call any function
+            //print("하이!")
+            completion("Main Screen")
+        }
+    }
+    
 }
 
